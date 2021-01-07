d847af92e30f28c6f4b3f1b2611677ee68b6d489
feyifoluwa <abioyefeyifoluwa@gmail.com>
2021-01-07 17:22:03 +0100

    add project files

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: {}
   },
   mutations: {
+    assignData(state, data){
+      console.log(data)
+    }
   },
   actions: {
+    async fetchData({commit}){
+      try{
+        const response = await fetch(`https://hirng-x2021.glitch.me/api`);
+        const body = await response.json();
+
+        commit('assignData', body);
+      }catch(err){
+        console.log(err)
+      }
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import {mapState} from 'vuex'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+
+    }
+  },
+
+
+  beforeCreate() {
+    this.$store.dispatch("fetchData");
+  },  
+  computed:{
+    ...mapState({
+      
+    })
   }
 }
 </script>
